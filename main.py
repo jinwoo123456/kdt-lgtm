b8b8bff4b33512adce3e861964017da5f25bcf5f
jinwoo123456 <rlawlsdn1020@gmail.com>
2024-10-16 17:00:04 +0900

    커밋

---
+++ main.py
@@ -0,0 +1,16 @@
+import click
+from lgrm import core
+@click.command()
+def cli():
+    """LGTM 이미지 생성 도구"""
+    lgtm()
+    click.echo('lgtm')  # 동작 확인용
+
+
+def lgtm():
+    # 여기에 로직을 추가함
+    pass
+
+
+if __name__ == '__main__':
+    cli()
